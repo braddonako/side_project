48b918210959f7b349489f6af7a4a5841771a805
Brad Donakowski <braddonako@gmail.com>
2020-09-18 16:40:42 -0600

    spun up react app

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# side_project
+something to ef around with react
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Stuff
+    </div>
+  );
+}
+
+export default App;
